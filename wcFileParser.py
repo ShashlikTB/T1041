c732995609c6135482a19c79a650fad80a6b1529
Thomas Anderson <tba9h@virginia.edu>
2014-04-14 14:44:52 -0400

    Initial Working Wire Chamber Parser, will add sorting to it

--- wcFileParser.py
+++ wcFileParser.py
@@ -24,127 +24,168 @@ class wcParser:
         except IOError as e:
             logger.Warn("Unable to open %s, %s" % (filename, e))
 
-        
-        self.spills = []
         self.inSpill = False
+        self.spills = []
+        self.currentModule = None 
+        self.currentSpill = None 
+        self.currentEvent = None 
+        try: 
+            self.processFile()
+        except Exception as e:
+            logger.Warn("Couldn't process file %s" % e)
+
+
+
+    def wcSpill(self, line):
+        spill = re.split(' +', line.strip())
+        if len(spill) == 2:
+            if self.currentSpill:
+                self.spills.append(currentSpill)
+            self.currentSpill = {'id':spill[1],
+                                 'events':[]}
+        else:
+            print "Bad Spill Line: %s" % line
+            return None 
+
+
+    def wcStartDate(self, line): 
+        data = re.split(' +', line.strip())
+        date = None
+        if len(data) != 4: 
+            logger.Warn("Bad start date: %s" % line)
+        else: 
+            date = time.strptime(' '.join(data[1:]), "%d %m %y")
+
+
+        print date
+        return date
+
+    def wcStartTime(self, line):
+        data = re.split(' +', line.strip())
+        t = None
+        if len(data) != 4:
+            logger.Warn("Bad start time: %s" % line)
+        else:
+            t = time.strptime(' '.join(data[1:]), "%H %M %S")
+
+        print t
+        return t
+
+    def wcTotalTime(self, line):
+        data = re.split(' +', line.strip())
+        datetime = None
+        if len(data) != 3:
+            logger.Warn("Bad total time: %s" % line)
+        else:
+            datetime = time.strptime(' '.join(data[1:]), "%d/%m/%y %H:%M:%S")
+
+        print datetime
+        return datetime
+
+    def wcEvent(self, line):
+        if self.currentEvent:
+            # New Event 
+            self.currentSpill['events'].append(self.currentEvent)
+
+
+        data = re.split(' +', line.strip())
+        wcrun = data[1]
+        wcspill = data[2]
+
+        self.currentEvent = {'modules':{},
+                      'time':None,
+                      'id':data[1], 
+                      'spill':data[2]}
 
 
         
 
+    def wcEventTime(self, line):
+        data = re.split(' +', line.strip())
+        self.currentEvent['time'] = (data[1], data[2])
 
-    
+        
 
-def wcSpill(line):
-    spill = re.split(' +', line.strip())
-    if len(spill) == 2:
-        return spill[1]
-    else:
-        print "Bad Spill Line: %s" % line
-        return None 
-
-
-def wcStartDate(line): 
-    data = re.split(' +', line.strip())
-    date = None
-    if len(data) != 4: 
-        logger.Warn("Bad start date: %s" % line)
-    else: 
-        date = time.strptime(' '.join(data[1:]), "%d %m %y")
-
-
-    print date
-    return date
-
-def wcStartTime(line):
-    data = re.split(' +', line.strip())
-    t = None
-    if len(data) != 4:
-        logger.Warn("Bad start time: %s" % line)
-    else:
-        t = time.strptime(' '.join(data[1:]), "%H %M %S")
-
-    print t
-    return t
-
-def wcTotalTime(line):
-    data = re.split(' +', line.strip())
-    datetime = None
-    if len(data) != 3:
-        logger.Warn("Bad total time: %s" % line)
-    else:
-        datetime = time.strptime(' '.join(data[1:]), "%d/%m/%y %H:%M:%S")
-
-    print datetime
-    return datetime
-
-def wcEvent(line):
-    data = re.split(' +', line.strip())
-    wcrun = data[1]
-    wcspill = data[2]
-
-    return (wcrun, wcspill)
-
-def wcEventTime(line):
-    data = re.split(' +', line.strip())
-    
-    return None
-
-def wcModule(line):
-    data = re.split(' +', line.strip())
-    module = 0
-    if len(data) != 2:
-        logger.Warn("Bad Module Line: %s" % line)
-    else:
-        module = int(data[1])
-    return module
-
-
-def wcChannel(line):
-    data = re.split(' +', line.strip())
-    channel = 0
-    count = 0
-    if len(data) != 3:
-        logger.Warn("Bad channel Line: %s" % line)
-    else:
-        channel = data[1]
-        count = data[2]
-
-    return (channel, count)
-
-wcMapper = { 
-    'SPILL':wcSpill,
-    'SDATE':wcStartDate,
-    'STIME':wcStartTime,
-    'TTIME':wcTotalTime,
-    'EVENT':wcEvent,
-    'ETIME':wcEventTime,
-    'Module':wcModule,
-    'Channel':wcChannel,
-}
-
-
-
-
-def processWireChamber(filename): 
-    wc = None
-    try:
-        if filename.endswith(".txt") or filename.endswith(".dat"):
-            wc = open(filename, "r")
-        elif filename.endswith(".bz2"):
-            wc = bz2.BZ2File(filename, "r")
+    def wcModule(self, line):
+        data = re.split(' +', line.strip())
+        module = 0
+        if len(data) != 2:
+            logger.Warn("Bad Module Line: %s" % line)
         else:
-            logger.Warn('Unrecognized filename extension')
-            return None
-    except IOError as e:
-        logger.Warn("Unable to open %s, %s" % (filename, e))
-        raise
+            module = int(data[1])
 
-    chamberHits = {}
-    for line in wc: 
-        line = line.strip()
-        data = re.split(' +', line)
-        fn = wcMapper[data[0]]
-        fn(line)
+        if self.currentModule:
+            self.currentEvent['modules'][self.currentModule['id']] = self.currentModule
+        
+        self.currentModule = {'id':module,
+                              'channels':{}}
 
 
-    return chamberHits
+    def wcChannel(self, line):
+        data = re.split(' +', line.strip())
+        channel = 0
+        count = 0
+        if len(data) != 3:
+            logger.Warn("Bad channel Line: %s" % line)
+        else:
+            channel = data[1]
+            count = data[2]
+        
+        self.currentModule['channels'][channel] = count
+
+            
+
+    wcMapper = { 
+        'SPILL':wcSpill,
+        'SDATE':wcStartDate,
+        'STIME':wcStartTime,
+        'TTIME':wcTotalTime,
+        'EVENT':wcEvent,
+        'ETIME':wcEventTime,
+        'Module':wcModule,
+        'Channel':wcChannel,
+        }
+
+
+
+
+    def processFile(self): 
+        if not self.wc:
+            raise 
+
+        for line in self.wc: 
+            line = line.strip()
+            data = re.split(' +', line)
+            fn = self.wcMapper[data[0]]
+            print "Executing %s" % data[0]
+            fn(self,line)
+
+        if self.currentSpill:
+            self.spills.append(self.currentSpill)
+        
+
+
+
+    def processWireChamber(self): 
+        wc = None
+        try:
+            if filename.endswith(".txt") or filename.endswith(".dat"):
+                wc = open(filename, "r")
+            elif filename.endswith(".bz2"):
+                wc = bz2.BZ2File(filename, "r")
+            else:
+                logger.Warn('Unrecognized filename extension')
+                return None
+        except IOError as e:
+            logger.Warn("Unable to open %s, %s" % (filename, e))
+            raise
+
+        chamberHits = {}
+        for line in wc: 
+            line = line.strip()
+            data = re.split(' +', line)
+            fn = wcMapper[data[0]]
+            fn(line)
+
+
+        return chamberHits
--- wcTest.py
+++ wcTest.py
@@ -1,7 +1,22 @@
 from wcFileParser import * 
+from operator import itemgetter, attrgetter
 
 
 
+p = wcParser('wc.dat')
+spills = p.spills
+spills = sorted(spills, key=itemgetter('id'))
+for spill in spills:
+    print "Spill %s has %s events." % (spill['id'], len(spill['events']))
+    events = spill['events']
 
-f = 'wc.dat'
-processWireChamber(f)
+    # Sort the events based on the second part of the tuple stored in the time entry
+    fn = lambda x: itemgetter('time')(x)[1]
+    events = sorted(events, key=fn)
+    for event in events:
+        print "Spill %s, Event %s, Start Time: %s" % (event['spill'], event['id'], event['time'])
+        print 'Modules %s' % event['modules'].keys()
+    for event in events:
+        print "Event %s, Start Time: %s" % (event['id'], event['time'][1])
+
+        
