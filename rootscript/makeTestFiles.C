aab33548b5e891af3f00b84e6e547fdc623f166c
Bob Hirosky <bob.hirosky@gmail.com>
2014-08-02 18:55:02 -0400

    make smaller test file

--- rootscript/makeTestFiles.C
+++ rootscript/makeTestFiles.C
@@ -2,8 +2,8 @@ void makeTestFiles(){
 TString in,out;
   gROOT->ProcessLine(".L rootscript/copyTBTree.C");
   in="/raids/nas0/testbeam2/reco/rec_capture_20140802_160330_reco.root";
-  out="data/200events_16Gev_electrons_20140802_160330_reco.root";
-  copyTBTree(in,out,200);
+  out="data/20events_16Gev_electrons_20140802_160330_reco.root";
+  copyTBTree(in,out,20);
 }
 
 
